9850affa42aeecad8f6f5e9ee1979d193e24d10d
Michael Tong <michaelyuyangtong@gmail.com>
2022-01-27 22:13:26 +0800

    Added axios and built task interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import Task from "./components/Task";
+
 function App() {
-  return <div className="w-screen h-screen bg-white overflow-hidden"></div>;
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getTasks").then((res) => {
+      setTasks(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="w-screen h-screen bg-white overflow-hidden">
+      <div className="mx-auto h-full pt-[52px] pb-8 max-w-xl">
+        {tasks.map((task) => {
+          return <Task title={task.taskName} completed={task.completed} />;
+        })}
+        <button
+          className="mt-4 w-full text-sm text-gray-500 h-8 border-[2px] border-dashed rounded-md bg-transparent border-gray-200 hover:border-gray-300 hover:bg-gray-100 transition-colors grid place-items-center"
+          onClick={null}
+        >
+          + Add Task
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Task.js
@@ -0,0 +1,7 @@
+export default function Task({ title, completed }) {
+  return (
+    <div className="w-full p-2 flex items-center bg-gray-100 rounded-md">
+      <div className="w-6 h-6 rounded-md border-[2px] border-gray-300"></div>
+    </div>
+  );
+}
